a83c8c39b907b7fa841d94b07352e4e220a0393f
liubovpo <lyubov.html@gmail.com>
2023-11-17 18:57:54 +0100

    minor changes

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import cors from "cors";
 
 import connectDB from "./mongodb/connect.js";
 import postRoutes from "./routes/postRoutes.js"
-import dalleRoutes from "./routes/dalleRoutes.js"
+import aiRoutes from "./routes/aiRoutes.js"
 
 dotenv.config();
 
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
 app.use('/api/v1/post', postRoutes)
-app.use('/api/v1/dalle', dalleRoutes)
+app.use('/api/v1/ai', aiRoutes)
 
 app.get("/", async (req, res) => {
   res.send("Hello from DALL-E!");
